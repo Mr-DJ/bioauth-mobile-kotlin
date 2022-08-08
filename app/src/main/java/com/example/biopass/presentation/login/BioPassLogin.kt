c68f5bed28124d58157cf1e04d9fe30d600b6ca1
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-08-08 11:02:09 +0530

    BioPass v0.0.1

--- app/src/main/java/com/example/biopass/presentation/login/BioPassLogin.kt
+++ app/src/main/java/com/example/biopass/presentation/login/BioPassLogin.kt
@@ -1,14 +1,16 @@
 package com.example.biopass.presentation.login
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun BioPassLogin() {
@@ -24,6 +26,24 @@ fun BioPassLogin() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        OutlinedTextField(value = adminNameValue.value, onValueChange = {adminNameValue.value = it}, )
+        OutlinedTextField(
+            value = adminNameValue.value,
+            onValueChange = { adminNameValue.value = it },
+            modifier = Modifier.width(
+                LocalConfiguration.current.screenWidthDp.dp - 40.dp
+            )
+        )
+        Spacer(modifier = Modifier.padding(25.dp))
+        OutlinedTextField(
+            value = passWordValue.value,
+            onValueChange = { passWordValue.value = it },
+            modifier = Modifier.width(
+                LocalConfiguration.current.screenWidthDp.dp - 40.dp
+            )
+        )
+        
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Sign In")
+        }
     }
 }
