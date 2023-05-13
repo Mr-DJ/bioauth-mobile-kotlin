b499286b6b4392d6f2b84db0d1ebb0d0625eafb5
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2023-05-13 17:41:45 +0530

    1st release proper codes

--- app/src/main/java/com/example/biopass/constant/Constants.kt
+++ app/src/main/java/com/example/biopass/constant/Constants.kt
@@ -4,7 +4,6 @@ object Constants {
     const val PRODUCTIONBASEURL = "https://biopasssever-production.up.railway.app/"
     const val TESTBASEURL = "http://10.0.2.2:3000/"
     const val SAMHOUSETESTURL = "http://192.168.0.117:3000/"
-    const val WOMANSAFETYURL = "http://192.168.0.101:3000/"
     const val PRODUCTIONSOCKET = "https://biopasssever-production.up.railway.app/"
     const val TESTSOCKET = "http://10.0.2.2:3000/"
     const val GETWEBSITE = "biopass/"
